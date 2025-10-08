d54c9d2858bceb61e7f98bf1ae7dc2266cd6e155
ManiaAmaeOvo <maniaamae@ManiaAmaeOvoMacBook-Pro.local>
2025-10-08 17:07:37 +0800

    适配了win系统

--- run.py
+++ run.py
@@ -1,4 +1,12 @@
+import sys
+import asyncio
 import uvicorn
 
+
+if sys.platform == 'win32':
+    asyncio.set_event_loop_policy(asyncio.WindowsProactorEventLoopPolicy())
+
+from app.main import app
+
 if __name__ == "__main__":
-    uvicorn.run("app.main:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run(app, host="127.0.0.1", port=8000)
